9d4e5c3eadf6926047f2214e243285ba461b4c78
chriswu727 <yichenwu2024@u.northwestern.edu>
2024-09-05 22:52:23 -0500

    added travis config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. AWS run test2
+          Edit <code>src/App.js</code> and save to reload. AWS run test23
         </p>
         <a
           className="App-link"
